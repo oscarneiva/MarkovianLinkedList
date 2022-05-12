63cb25c9303ad9547f5e3a543a77d84613cc40ab
oscarneiva <oscarneivaeneto@gmail.com>
2022-05-11 22:19:30 -0300

    Difference between using pointer and no pointer

--- main/Main.go
+++ main/Main.go
@@ -15,15 +15,19 @@ type Line struct {
 	p2 Point
 }
 
-func (l Line) calculateDistance() float64{
-	return math.Sqrt(math.Pow(l.p1.x - l.p2.x,2) + math.Pow(l.p1.y - l.p2.y,2))
+func (l *Line) calculateDistance() float64 {
+	distance := math.Sqrt(math.Pow(l.p1.x-l.p2.x, 2) + math.Pow(l.p1.y-l.p2.y, 2))
+	l.p1.x = 1000
+	return distance
 }
+
+//func New(x1 float64, y1 float64, x2 float64, y2 float64) *Line{
+//	return &Line{Point{x1,y1}, Point{x2,y2}}
+//}
 
 func main() {
-	l := Line{Point{1,1}, Point{2,2}}
-	fmt.Println(l.calculateDistance())
+	//l := New(1,1,2,2)
+	l := Line{Point{1, 1}, Point{2, 2}}
+	fmt.Println(l.calculateDistance()) // Use values in the constructor to calculate distance
+	fmt.Println(l.calculateDistance()) // Print value after changing l.p1.x to 1000
 }
-
-
-
-
